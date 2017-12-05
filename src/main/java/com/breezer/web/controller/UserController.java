3c999863b6308d187db74ddd8e2b5f1335bdd929
PureAppCrystal <purecrystar@gmail.com>
2017-12-05 18:30:11 +0900

    delete useless file

--- src/main/java/com/breezer/web/controller/UserController.java
+++ src/main/java/com/breezer/web/controller/UserController.java
@@ -1,16 +1,10 @@
 package com.breezer.web.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.breezer.web.service.UserService;
-import com.breezer.web.vo.UserVo;
 
 @Controller("userController")
 @RequestMapping("/user")
@@ -19,23 +13,7 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@RequestMapping("/breezer_join_page")
-	public String breezerPage() {
-		return "user/breezer_join";
-	}
 	
-	@RequestMapping( value="/breezer_join", method=RequestMethod.POST)
-	public String breezerJoin( HttpServletRequest request, @ModelAttribute UserVo vo) {
-		
-		//userService.joinMessage(vo);
-		
-		
-		HttpSession session = request.getSession( true );
-		session.setAttribute( "authUser", vo );
-		
-		
-		return "user/joinsuccess";
-	}
 	
 	
 
